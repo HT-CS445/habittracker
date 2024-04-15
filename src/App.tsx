41a33157abb7b313eba30e63a7115aa51a591329
Alaina <alaina.porquis@gmail.com>
2024-04-14 21:59:54 -0700

    LoginRegisterPages

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { accessibilityOutline, sparklesOutline, barChartOutline, sparkles } from 'ionicons/icons';
 import Tab1 from './pages/Home';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -59,21 +59,21 @@ const App: React.FC = () => (
             <Tab3 />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/Login" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={accessibilityOutline} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon aria-hidden="true" icon={sparklesOutline} />
+            <IonLabel>Add</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon aria-hidden="true" icon={barChartOutline} />
+            <IonLabel>Analyze</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
-import { IonContent, IonFooter, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { logInOutline, personCircleOutline } from 'ionicons/icons';
+import LoginImage from '../assets/LoginImage.png'; 
 
 const Login: React.FC = () => {
     const doLogin = (event: any) => {
@@ -8,21 +10,33 @@ const Login: React.FC = () => {
     }
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar color = {'success'}>
-                    <IonTitle>Log In</IonTitle>
+            <IonHeader className= "ion-text-center">
+                <IonToolbar color = {'primary'}>
+                    <IonTitle>Habit Tracker</IonTitle>
                 </IonToolbar>
             </IonHeader>
-
-            <IonContent className="ion-padding">
-                Content goes here
+            <IonContent class="ion-padding">
+            <IonContent scrollY ={false}>
+                <div className="ion-text-center ion-padding-top ion-margin-top">
+                <img src={LoginImage} alt='healthy habits icon'/>
+                </div>
+                <IonCard>
+                    <IonCardContent class = "ion-padding">
+                        <form onSubmit={doLogin}>
+                            <IonInput label="email" type='email' fill='outline' labelPlacement='floating' > </IonInput>
+                            <IonInput className="ion-margin-top" fill='outline' label="password" type='password' labelPlacement='floating' > </IonInput>
+                            <IonButton routerLink="/tab1" color={'secondary'} type='submit' expand='block' className='ion-margin-top'>
+                                Login
+                                <IonIcon icon={logInOutline} slot="end" />    
+                            </IonButton>
+                            <IonButton routerLink="/register" color={'tertiary'} type='button' expand='block'>Create Account
+                            <IonIcon icon={personCircleOutline} slot="end" />   
+                            </IonButton> 
+                        </form>
+                    </IonCardContent>
+                </IonCard>
             </IonContent>
-
-            <IonFooter>
-                <IonToolbar>
-                    A healthy life is built one habit at a time.
-                </IonToolbar>
-            </IonFooter>
+            </IonContent>
         </IonPage>
     );
 };
